fb537aa97677bbc98c21fac7f11b20ad189efaad
mjay <michael@merged.group>
2023-05-20 22:28:39 -0400

    added credentials to axios

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: process.env.VUE_APP_API_BASE_URL });
+const api = axios.create({
+  baseURL: process.env.VUE_APP_API_BASE_URL,
+  withCredentials: true,
+});
 
 export default api;
