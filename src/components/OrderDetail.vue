7e47a94d9b283e610fc86d1d7d71945e964c2c39
mjay <michael@merged.group>
2023-06-01 22:22:04 -0400

    updated OrderDetail

--- src/components/OrderDetail.vue
+++ src/components/OrderDetail.vue
@@ -50,6 +50,34 @@
         {{ quantity }} x {{ name }}
       </li>
     </ul>
+
+    <div class="flex space-x-4 mt-4">
+      <div>
+        <label for="status" class="block text-sm font-medium text-gray-700 mb-1"
+          >Status</label
+        >
+        <select
+          id="status"
+          class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          v-model="order.status"
+        >
+          <option>New</option>
+          <option>In Progress</option>
+          <option>Complete</option>
+          <option>Cancelled</option>
+        </select>
+      </div>
+
+      <div class="pt-6">
+        <button
+          class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+          disabled
+        >
+          <span>Create Lead</span>
+        </button>
+        <span class="text-gray-500 text-xs italic">Coming Soon</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -74,6 +102,16 @@ export default defineComponent({
     onMounted(fetchOrder);
 
     watch(() => props.orderId, fetchOrder);
+
+    watch(
+      () => order.value?.status,
+      async (newStatus, oldStatus) => {
+        // if (newStatus !== oldStatus) {
+        //   await api.put(`/orders/${props.orderId}`, { status: newStatus });
+        // }
+        return;
+      }
+    );
 
     const formatOrderNumber = (number: string) => {
       return `#${number.slice(0, 3)} ${number.slice(3)}`;
